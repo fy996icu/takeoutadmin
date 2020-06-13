77186f853272204a3eae9859e7a2e8034738237d
fanyong <2120084903@qq.com>
2020-06-13 14:25:04 +0800

    项目优化

--- config/index.js
+++ config/index.js
@@ -13,7 +13,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    // host: 'localhost', // can be overwritten by process.env.HOST
+    host: '172.16.4.195',
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
